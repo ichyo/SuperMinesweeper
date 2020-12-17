42c7898a0c211ca93f773bd1675d5c41675079d7
ichyo <ichyo00@gmail.com>
2020-12-17 23:53:48 +0900

    Add run_test.sh

---
+++ run_test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -eu
+
+TL=7500 # msec
+CASE=1000
+
+THREADS=6
+BASE=1000000
+
+SOURCE=./submission.cpp
+EXEC=./submission
+
+OUTPUT=/tmp/MM/$(date '+%Y%m%d-%H%M%S')
+[ -f "$OUTPUT" ] && { echo "$OUTPUT exists"; exit 1; }
+
+make
+
+mkdir -p "$OUTPUT"
+
+echo "source_file: ${SOURCE}" | tee -a $OUTPUT/info.yml
+echo "source_md5sum: $(md5sum "$SOURCE" | cut -d ' ' -f 1)" | tee -a $OUTPUT/info.yml
+echo "executable: ${EXEC}" | tee -a $OUTPUT/info.yml
+echo "threads: ${THREADS}" | tee -a $OUTPUT/info.yml
+echo "time_limit: ${TL}" | tee -a $OUTPUT/info.yml
+echo "first_seed: ${BASE}" | tee -a $OUTPUT/info.yml
+echo "case_num: ${CASE}" | tee -a $OUTPUT/info.yml
+echo "start_time: $(date '+%Y-%m-%d %H:%M:%S')" | tee -a $OUTPUT/info.yml
+
+echo ""
+echo $OUTPUT
+
+java -jar tester/tester.jar -exec $EXEC -seed $BASE+$CASE -novis -noSummary -timeLimit $TL -threads $THREADS -saveScores $OUTPUT/scores.txt -saveAll $OUTPUT/io > /dev/null
