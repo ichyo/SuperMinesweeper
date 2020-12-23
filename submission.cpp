b49e31555835f33f205f90a8c4d4becd6b48cbc0
ichyo <ichyo00@gmail.com>
2020-12-23 15:12:19 +0900

    Sort by size
    g++ -std=gnu++11 -D LOCAL_TEST -g -pg -O3 -o submission ./submission.cpp
    source_file: ./submission.cpp
    source_md5sum: 1a159db45e9d7eccf4368f8ec09c2aa6
    executable_md5sum: 6d873bc9a03725dc0da924eec41197ac
    executable: ./submission
    threads: 6
    time_limit: 5000
    first_seed: 1000000
    case_num: 1000
    start_time: 2020-12-23 15:10:54
    additional_args:
    
    /tmp/MM/20201223-151050
    end_time: 2020-12-23 15:12:09
    === score stats ===
    avg: 0.8720
    std: 0.2742
    min: 0.0091
    05t: 0.1628
    25t: 0.9840
    50t: 1.0000
    75t: 1.0000
    === param stats ===
    d=1
      cnt: 128
      avg: 0.6080
      25t: 0.2145
      50t: 0.7541
      75t: 0.9928
    d=2
      cnt: 143
      avg: 0.7795
      25t: 0.6210
      50t: 0.9678
      75t: 0.9989
    d=4
      cnt: 159
      avg: 0.9761
      25t: 1.0000
      50t: 1.0000
      75t: 1.0000
    d=5
      cnt: 147
      avg: 0.9795
      25t: 1.0000
      50t: 1.0000
      75t: 1.0000
    d=8
      cnt: 149
      avg: 0.7664
      25t: 0.4970
      50t: 0.9979
      75t: 1.0000
    d=9
      cnt: 140
      avg: 0.9785
      25t: 1.0000
      50t: 1.0000
      75t: 1.0000
    d=10
      cnt: 134
      avg: 0.9875
      25t: 1.0000
      50t: 1.0000
      75t: 1.0000
    === meta stats ===
    reason=stop
      cnt: 308
      avg: 0.6884
      25t: 0.3267
      50t: 0.8962
      75t: 0.9935
    reason=timeout
      cnt: 20
      avg: 0.1114
      25t: 0.0255
      50t: 0.0663
      75t: 0.1201
    reason=win
      cnt: 672
      avg: 0.9788
      25t: 1.0000
      50t: 1.0000
      75t: 1.0000
    max_score
      avg: 0.8765
      25t: 0.9840
      50t: 1.0000
      75t: 1.0000
    === norm score stats ===
    avg: 0.9173
    std: 0.2024
    min: 0.0734
    05t: 0.3712
    25t: 0.9962
    50t: 1.0000
    75t: 1.0000
    d=1
      avg: 0.6836
      25t: 0.3752
      50t: 0.8228
    d=2
      avg: 0.8478
      25t: 0.7717
      50t: 0.9910
    d=4
      avg: 0.9901
      25t: 1.0000
      50t: 1.0000
    d=5
      avg: 0.9932
      25t: 1.0000
      50t: 1.0000
    d=8
      avg: 0.8957
      25t: 0.9878
      50t: 1.0000
    d=9
      avg: 0.9893
      25t: 1.0000
      50t: 1.0000
    d=10
      avg: 0.9938
      25t: 1.0000
      50t: 1.0000
    Written into /tmp/MM/20201223-151050/scores.csv

--- submission.cpp
+++ submission.cpp
@@ -26,6 +26,11 @@
 
 using namespace std;
 
+const int MAX_N = 50;
+
+const int CANCEL_LIMIT = MAX_RUNTIME / 14;
+
+
 /*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
 
 To the extent possible under law, the author has dedicated all copyright
@@ -164,10 +169,6 @@ namespace rng {
     }
 }
 
-const int MAX_N = 50;
-
-const int CANCEL_LIMIT = MAX_RUNTIME / 14;
-
 
 using Pos = pair<int, int>;
 
@@ -256,6 +257,10 @@ struct Params {
     int N;
     int M;
     int D;
+
+    double bomb_ratio() {
+        return (double)M/N/N;
+    }
 };
 
 class Cell {
@@ -723,25 +728,6 @@ class Solver {
         }
 
 
-        /*
-        for(int i = 0; i < points.size(); i++) {
-            for (int j : p2c[i]) {
-                cerr << j << " ";
-            }
-            cerr << endl;
-        }
-        cerr << "---" << endl;
-        for(auto p : counter) {
-            if (p.second >= 2) {
-                cerr << "[";
-                for(auto x : p.first) {
-                    cerr << x << " ";
-                }
-                cerr << "] " << p.second << endl;
-            }
-        }
-        */
-
         vector<vector<int>> point_groups;
         {
             map<vector<int>, int> group_id;
@@ -758,6 +744,22 @@ class Solver {
                 point_groups[group_id[p2c[i]]].push_back(i);
             }
         }
+        sort(point_groups.begin(), point_groups.end(), [&p2c](const vector<int>& x, const vector<int>& y){
+            if (p2c[x[0]].size() != p2c[y[0]].size()) {
+                return p2c[x[0]].size() > p2c[y[0]].size();
+            }
+            return x.size() > y.size();
+        });
+
+#ifdef LOCAL
+        for(int i = 0; i < point_groups.size(); i++) {
+            cerr << "[size: " << point_groups[i].size() << "] ";
+            for (int j : p2c[point_groups[i][0]]) {
+                cerr << j << " ";
+            }
+            cerr << endl;
+        }
+#endif
 
         _dfs_p_count = vector<int>(point_groups.size());
         _dfs_p2c = vector<vector<int>>(point_groups.size());
